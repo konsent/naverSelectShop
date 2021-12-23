a598cb087afa5d688d5c7bce80e478aeee292932
Yonghee <konsent2@gmail.com>
2021-12-23 22:17:21 +0900

    211223 Scheduler

--- src/main/java/com/sparta/week04/service/ProductService.java
+++ src/main/java/com/sparta/week04/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.sparta.week04.service;
 
+import com.sparta.week04.models.ItemDto;
 import com.sparta.week04.models.Product;
 import com.sparta.week04.models.ProductMypriceRequestDto;
 import com.sparta.week04.models.ProductRepository;
@@ -21,5 +22,12 @@ public class ProductService {
         );
         product.update(requestDto);
         return id;
+    }
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto){
+        Product product = productRepository.findById(id).orElseThrow(
+                ()-> new IllegalArgumentException("아이디가 존재하지않습니다")
+        );
+        product.updateByItemDto(itemDto);
     }
 }
